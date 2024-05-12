0946afd09295b5b01f46dfe1f94c49add9d9e885
Xero1000 <alex.heil@comcast.net>
2024-05-12 08:53:22 -0400

    Make an example class for hashing keys

--- Data Structures Part 1/Hash Tables/index.js
+++ Data Structures Part 1/Hash Tables/index.js
@@ -34,6 +34,43 @@ class CharFinder {
 }
 
 let chFinder = new CharFinder()
-console.log(chFinder.findFirstNonRepeatedChar("a green apple"))
-console.log(chFinder.findFirstRepeatedChar("a green apple"))
+// console.log(chFinder.findFirstNonRepeatedChar("a green apple"))
+// console.log(chFinder.findFirstRepeatedChar("a green apple"))
+
+class HashExample {
+    #arraySize = 0;
+
+    constructor(size) {
+        this.#arraySize = size
+    }
+
+    hash(key) {
+        let index = 0;
+
+        if (typeof key === "number") {
+            index = this.#hashInteger(key)
+        }
+        if (typeof key === "string") {
+            index = this.#hashString(key)
+        }
+        return index
+    }
+
+    #hashInteger(key) {
+        let hashValue = key % this.#arraySize
+        return hashValue;
+    }
+
+    #hashString(key) {
+        let hashValue = 0; 
+        for (let i = 0; i < key.length; i++) {
+            hashValue += key.charCodeAt(i)
+        }
+        hashValue %= this.#arraySize
+        return hashValue
+    }
+}
+
+let hash = new HashExample(100)
+console.log(hash.hash("123456-A"))
 
