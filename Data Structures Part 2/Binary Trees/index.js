3dd859bcc352e5fd62e162f4d426d0959a261d4c
Xero1000 <alex.heil@comcast.net>
2024-05-14 09:24:49 -0400

    Implement pre, in, and post order functions for binary search tree class

--- Data Structures Part 2/Binary Trees/index.js
+++ Data Structures Part 2/Binary Trees/index.js
@@ -55,6 +55,45 @@ class BinarySearchTree {
         }
         return false
     }
+
+    traversePreOrder() {
+        this.#traversePreOrder(this.#root)
+    }
+
+    #traversePreOrder(root) {
+        if (root === null) 
+            return
+
+        console.log(root.value)
+        this.#traversePreOrder(root.leftChild)
+        this.#traversePreOrder(root.rightChild)
+    }
+
+    traverseInOrder() {
+        this.#traverseInOrder(this.#root)
+    }
+    
+    #traverseInOrder(root) {
+        if (root === null) 
+            return
+
+        this.#traverseInOrder(root.leftChild)
+        console.log(root.value)
+        this.#traverseInOrder(root.rightChild)
+    }
+
+    traversePostOrder() {
+        this.#traversePostOrder(this.#root)
+    }
+
+    #traversePostOrder(root) {
+        if (root === null)
+            return
+
+        this.#traversePostOrder(root.leftChild)
+        this.#traversePostOrder(root.rightChild)
+        console.log(root.value)
+    }
 }
 
 let bst = new BinarySearchTree()
@@ -65,4 +104,17 @@ bst.insert(1)
 bst.insert(6)
 bst.insert(8)
 bst.insert(10)
-console.log(bst.find(8))
+//console.log(bst.find(8))
+bst.traversePostOrder()
+
+
+// f(3)
+//   3 * f(2)
+//         2 * f(1)
+//               1 * 1
+function factorial(n) {
+    if (n === 0) return 1 // base condition
+    return n * factorial(n - 1)
+}
+
+//console.log(factorial(5))
